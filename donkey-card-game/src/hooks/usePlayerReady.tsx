f3d660a0b187dc49bc6fbaaed93403c7eb2fbe29
BMidhun <midhunbabygeorge@gmail.com>
2023-01-15 20:06:47 +0530

    remove isLoading variable

--- donkey-card-game/src/hooks/usePlayerReady.tsx
+++ donkey-card-game/src/hooks/usePlayerReady.tsx
@@ -4,9 +4,6 @@ const PLAYER_HAND_LENGTH = 13;
 
 export default function usePlayerReady(cb:() => void) {
     const [numOfCardsLoaded,setNumOfCardsLoaded] = useState(0);
-    const isLoading = numOfCardsLoaded !== PLAYER_HAND_LENGTH;
-
-    console.log("isLoading::", isLoading);
 
     function incrementNumOfCardsLoaded() {
         setNumOfCardsLoaded(prev => prev + 1);
@@ -19,5 +16,5 @@ export default function usePlayerReady(cb:() => void) {
     }, [numOfCardsLoaded,cb])
 
 
-    return {incrementNumOfCardsLoaded, isLoading};
+    return {incrementNumOfCardsLoaded};
 }
